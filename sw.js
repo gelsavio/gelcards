720de3c4884576e87d57de621e9fed3f712986f6
gelsavio <gelsavio@gmail.com>
2026-06-17 11:31:24 -0300

    v4.0.2: Ajustes de versão e preparação para novos commits

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ATENÇÃO: Toda vez que você atualizar o app, mude este número (ex: v2, v3, v4)
-const CACHE_NAME = 'gelcifras-cache-v400';
+const CACHE_NAME = 'gelcifras-cache-v402';
 
 const ASSETS_TO_CACHE = [
     '/',
